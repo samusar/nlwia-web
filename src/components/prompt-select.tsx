a501eba2809dc8cc949f934eb5d9d53c21c7601e
Samuel Monção <samu.henr@gmail.com>
2023-09-13 23:12:26 -0300

    feat: Finish one part of project

---
+++ src/components/prompt-select.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { api } from "@/lib/api";
+
+type TPromptData = {
+  id: string;
+  title: string;
+  template?: string;
+}
+
+type PromptSelectProps = {
+  onPromptSelected: (template: string) => void;
+}
+
+export function PromptSelect({ onPromptSelected }: PromptSelectProps) {
+  const [prompts, setPrompts] = useState<TPromptData[]>([]);
+
+  useEffect(() => {
+    api.get('/prompts').then(({ data }) => setPrompts(data));
+  }, []);
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts.find(prompt => prompt.id === promptId);
+
+    if(!selectedPrompt) return;
+
+    onPromptSelected(selectedPrompt.template || '');
+  }
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts.map(prompt => (
+          <SelectItem key={prompt.id} value={prompt.id}>{prompt.title}</SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
